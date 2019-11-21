ecbbb9e73a3583126e8f9c69d25c10bfdcd34a1c
Ricky Rojas <rickyrojas19@gmail.com>
2019-11-21 14:17:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ function map(array, func) {
   return array.map(func)
 }
 
-function reduce(array, startingPoint=0) {
-  const total = array.reduce((startingPoint, currentVal) => startingPoint += currentVal)
-  
-  return total
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
 }
